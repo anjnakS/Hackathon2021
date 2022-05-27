6c299201aac66c8b337a83ace5081d51256dfcae
anjnakS <66026895+anjnakS@users.noreply.github.com>
2022-05-27 10:54:59 -0600

    Update innovationLab.sh

--- innovationLab.sh
+++ innovationLab.sh
@@ -11,9 +11,9 @@ echo "########################## "
 echo -e  "Install Maven"
 echo "######################### "
 sudo wget https://apache.osuosl.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
-tar -xvzf apache-maven-3.8.4-bin.tar.gz
-sudo mv apache-maven-3.8.4 /opt
-M2_HOME=/opt/apache-maven-3.8.4
+tar -xvzf apache-maven-3.8.5-bin.tar.gz
+sudo mv apache-maven-3.8.5 /opt
+M2_HOME=/opt/apache-maven-3.8.5
 PATH=$PATH:$M2_HOME/bin
 export PATH
 echo "########################## "
